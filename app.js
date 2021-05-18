02a7d8e35b389410b9fabe509147b062bb7f0fd9
Jonathan Chue <5141427+jchue@users.noreply.github.com>
2021-05-17 17:11:40 -0700

    Use environment variable for port

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import Koa from 'koa';
 
+require('dotenv').config();
+
 const app = new Koa();
+const port = process.env.PORT || '3000';
 
 app.use(async (ctx) => {
   ctx.body = 'Hello World';
 });
 
-app.listen(80);
+app.listen(port);
